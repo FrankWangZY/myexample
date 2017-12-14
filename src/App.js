f54e6ca013a427edd160de51370cf71885cb0946
frank <352202181@qq.com>
2017-12-14 20:37:47 +0800

    let go

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reloaddd.
         </p>
+          <p>
+              lets go
+          </p>
       </div>
     );
   }
